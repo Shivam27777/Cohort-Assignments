f33ad4781777e3ca17ecff72e43e18b7102e31af
Shivam <shivam27777@gmail.com>
2023-12-12 03:06:14 +0530

    Solved week2 hard Promise assignment

---
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -0,0 +1,15 @@
+/*
+ * Write a function that halts the JS thread (make it busy wait) for a given number of milliseconds.
+ * During this time the thread should not be able to do anything else.
+ * the function should return a promise just like before
+ */
+
+function sleep(milliseconds) {
+    const start = new Date().getTime()
+    while (new Date().getTime() - start < milliseconds) {}
+    return new Promise(function(resolves){ resolves()});
+}
+
+sleep(4000).then(()=>console.log("done"));
+
+module.exports = sleep;
